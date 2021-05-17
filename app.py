036b267916e25deb087a73ceedb34956b570d345
vladakytaieva <vlada.kitaeva2002@gmail.com>
2021-05-17 12:58:24 +0300

    optimising

--- app.py
+++ app.py
@@ -11,14 +11,18 @@ def index():
         xz = float(request.form['xz'])
         xp = float(request.form['xp'])
         xr = float(request.form['xr'])
-        xr2 = float(request.form['xr2'])
         res_1 = calc1(ci, gv, xz, xp, xr)
         res1 = list(map(lambda x: round(x, 2), res_1))
-        res_2 = calc2(ci, gv, xz, xp, xr, xr2)
-        res2 = list(map(lambda x: round(x, 2), res_2))
-        return render_template('index.html', res1=res1, res2=res2)
+        xr2  = request.form['xr2']
+        if xr2 != "":
+            xr2 = float(xr2)
+            res_2 = calc2(ci, gv, xz, xp, xr, xr2)
+            res2 = list(map(lambda x: round(x, 2), res_2))
+        else:
+            res2 = ()
+        return render_template('index.html', res1=res1, res2=res2, data=(ci, gv, xz, xp, xr, xr2))
     else:
-        return render_template('index.html', res1=(), res2=())
+        return render_template('index.html', res1=(), res2=(), data=())
  
 if __name__ == "__main__":
     app.run(debug=True)
